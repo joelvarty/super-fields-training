3f6ffe968745d8dd02e3f3d724c4b59e23905e14
Joel Varty <joel@agilitycms.com>
2023-05-25 13:56:08 -0400

    various fixes

--- versions/v1/components/BlockEditor.tsx
+++ versions/v1/components/BlockEditor.tsx
@@ -2,124 +2,128 @@ import React, { useState, useEffect, useRef, useMemo } from "react"
 import { contentItemMethods, useAgilityAppSDK, setHeight, getManagementAPIToken } from "@agility/app-sdk"
 import useOnScreen from "../hooks/useOnScreen"
 
-import EditorJS from '@editorjs/editorjs'
-import Embed from '@editorjs/embed'
-import Table from '@editorjs/table'
-import Paragraph from '@editorjs/paragraph'
-import Warning from '@editorjs/warning'
-import Code from '@editorjs/code'
-import Image from '@editorjs/image'
-import Raw from '@editorjs/raw'
-import Header from '@editorjs/header'
-import Quote from '@editorjs/quote'
-import Marker from '@editorjs/marker'
-import Delimiter from '@editorjs/delimiter'
-import InlineCode from '@editorjs/inline-code'
-import NestedList from '@editorjs/nested-list'
-import DragDrop from 'editorjs-drag-drop'
+import EditorJS from "@editorjs/editorjs"
+import Embed from "@editorjs/embed"
+import Table from "@editorjs/table"
+import Paragraph from "@editorjs/paragraph"
+import Warning from "@editorjs/warning"
+import Code from "@editorjs/code"
+import Image from "@editorjs/image"
+import Raw from "@editorjs/raw"
+import Header from "@editorjs/header"
+import Quote from "@editorjs/quote"
+import Marker from "@editorjs/marker"
+import Delimiter from "@editorjs/delimiter"
+import InlineCode from "@editorjs/inline-code"
+import NestedList from "@editorjs/nested-list"
+import DragDrop from "editorjs-drag-drop"
 
 const BlockEditor = ({ configuration }: { configuration: any }) => {
-    const { field, instance, contentItem } = useAgilityAppSDK()
-    const containerRef = useRef<HTMLIFrameElement>(null)
-    const blockRef = useRef<HTMLIFrameElement>(null)
-    const [editor, setEditor] = useState({})
-    const isVisible = useOnScreen(containerRef)
-    const blockValues = useMemo(() => contentItem?.values?.BlockEditorApp, [contentItem?.values?.BlockEditorApp])	
-		const [token, setToken] = useState()
+	const { field, instance, contentItem } = useAgilityAppSDK()
+	const containerRef = useRef<HTMLIFrameElement>(null)
+	const blockRef = useRef<HTMLIFrameElement>(null)
+	const [editor, setEditor] = useState({})
+	const isVisible = useOnScreen(containerRef)
+	const blockValues = useMemo(() => {
+		if (!contentItem?.values || !field) return ""
+		const str = contentItem?.values[field.name] || ""
+		return str
+	}, [contentItem?.values, field])
+	const [token, setToken] = useState()
 
-		// Get the ManagementAPIToken
-		useEffect(() => {
-			(async () => {
-				const token = await getManagementAPIToken()
-				setToken(token)
-			})()
-		}, [])
+	// Get the ManagementAPIToken
+	useEffect(() => {
+		;(async () => {
+			const token = await getManagementAPIToken()
+			setToken(token)
+		})()
+	}, [])
 
-		useEffect(() => {
-			if (!isVisible || !field || !token || !blockRef.current) return
+	useEffect(() => {
+		if (!isVisible || !field || !token || !blockRef.current) return
 
-			//if running locally after a hot-module replacement, don't reinitialize everything...
-			if (editor && Object.keys(editor).length > 0) return;
+		//if running locally after a hot-module replacement, don't reinitialize everything...
+		if (editor && Object.keys(editor).length > 0) return
 
-			const uploadImagePayload = {
-				guid: instance?.guid,
-				token,
-				assetFolder: configuration.assetFolder ? configuration.assetFolder : '/'
-			};
+		const uploadImagePayload = {
+			guid: instance?.guid,
+			token,
+			assetFolder: configuration.assetFolder ? configuration.assetFolder : "/"
+		}
 
-			console.log(uploadImagePayload)
-			const valueJS = typeof blockValues === 'string' ? JSON.parse(blockValues) : null;
+		console.log(uploadImagePayload)
+		const valueJS = typeof blockValues === "string" ? JSON.parse(blockValues) : null
 
-			const editorJS = new EditorJS({
-				autofocus: false, //setting this to true will not do anything because this is in an iframe
-				holder: blockRef.current,
-				placeholder: "📝 Enter text, paste images/embed urls, or select a block to add here...",
-				tools: {
-					table: Table,
-					paragraph: {
-							class: Paragraph,
-							inlineToolbar: true,
-					},
-					list: {
-							class: NestedList,
-							inlineToolbar: true
-					},
-					warning: Warning,
-					code: Code,
-					image: {
-							class: Image,
-							config: {
-									endpoints: {
-										byFile: '/api/image/uploadByFile',
-										byUrl: '/api/image/fetchByUrl'
-									},
-									additionalRequestData: { ...uploadImagePayload }
-							}
-					},
-					raw: Raw,
-					header: Header,
-					quote: Quote,
-					marker: Marker,
-					delimiter: Delimiter,
-					inlineCode: InlineCode,
-					embed: Embed
+		const editorJS = new EditorJS({
+			autofocus: false, //setting this to true will not do anything because this is in an iframe
+			holder: blockRef.current,
+			placeholder: "📝 Enter text, paste images/embed urls, or select a block to add here...",
+			tools: {
+				table: Table,
+				paragraph: {
+					class: Paragraph,
+					inlineToolbar: true
 				},
-				onChange: (e: any) => {
-					editorJS.save().then((v: any) => {
-							const valueJSON = JSON.stringify(v)
-							contentItemMethods.setFieldValue({ value: valueJSON })
+				list: {
+					class: NestedList,
+					inlineToolbar: true
+				},
+				warning: Warning,
+				code: Code,
+				image: {
+					class: Image,
+					config: {
+						endpoints: {
+							byFile: "/api/image/uploadByFile",
+							byUrl: "/api/image/fetchByUrl"
+						},
+						additionalRequestData: { ...uploadImagePayload }
+					}
+				},
+				raw: Raw,
+				header: Header,
+				quote: Quote,
+				marker: Marker,
+				delimiter: Delimiter,
+				inlineCode: InlineCode,
+				embed: Embed
+			},
+			onChange: (e: any) => {
+				editorJS.save().then((v: any) => {
+					const valueJSON = JSON.stringify(v)
+					contentItemMethods.setFieldValue({ value: valueJSON })
+				})
+			},
+			onReady: () => {
+				const blockSizeElm = document.querySelector<HTMLElement>("#container-element")
+				if (blockSizeElm) {
+					const observer = new ResizeObserver((entries) => {
+						const entry = entries[0]
+						if (!entry) return
+						let height = entry.contentRect.height
+						if (height < 400) height = 400
+
+						setHeight({ height})
 					})
-
-				},
-				onReady: () => {
-					const blockSizeElm = document.querySelector<HTMLElement>(".codex-editor")
-					if (blockSizeElm) {
-						const observer = new ResizeObserver((entries) => {
-							const entry = entries[0]
-							if (!entry) return
-							setHeight({ height: entry.contentRect.height + 50 })
-						})
-						observer.observe(blockSizeElm)
-					}
-
-					new DragDrop(editorJS);
-
-					if (valueJS && valueJS.blocks && valueJS.blocks.length > 0) {
-							editorJS.render(valueJS);
-					}
+					observer.observe(blockSizeElm)
 				}
-			});
 
-			setEditor(editorJS);
+				new DragDrop(editorJS)
 
-    }, [isVisible, field, editor, token])
+				if (valueJS && valueJS.blocks && valueJS.blocks.length > 0) {
+					editorJS.render(valueJS)
+				}
+			}
+		})
 
-    return (
-			<div className="bg-white py-2" ref={containerRef}>
-				<div ref={blockRef}></div>
-			</div>
-    );
+		setEditor(editorJS)
+	}, [isVisible, field, editor, token])
 
+	return (
+		<div className="bg-white p-3" ref={containerRef} id="container-element">
+			<div className="min-h-[200px] prose" ref={blockRef}></div>
+		</div>
+	)
 }
 
 export default BlockEditor
--- versions/v1/components/MarkdownEditor.tsx
+++ versions/v1/components/MarkdownEditor.tsx
@@ -1,49 +1,50 @@
 import { useAgilityAppSDK, contentItemMethods, setHeight } from "@agility/app-sdk"
 import React, { useState, useEffect, useRef, useMemo } from "react"
 import useOnScreen from "../hooks/useOnScreen"
-import SimpleMDE from "react-simplemde-editor";
+import SimpleMDE from "react-simplemde-editor"
 
 const MarkdownEditor = () => {
-  const { contentItem } = useAgilityAppSDK()
-  const containerRef = useRef<HTMLIFrameElement | null>(null)
-  const isVisible = useOnScreen(containerRef)
-  const markdownValues = useMemo(() => contentItem?.values?.MarkdownField, [contentItem?.values?.MarkdownField])
-  const [markdownHeight, setMarkdownHeight] = useState(500)
+	const { contentItem, field } = useAgilityAppSDK()
+	const containerRef = useRef<HTMLIFrameElement | null>(null)
+	const isVisible = useOnScreen(containerRef)
+	const markdownValues = useMemo(() => {
+		if (!contentItem?.values || !field) return ""
+		const str = contentItem?.values[field.name] || ""
+		return str
+	}, [contentItem?.values, field])
+	const [markdownHeight, setMarkdownHeight] = useState(500)
 
-  const onChange = (newValue: any) => {
-    const valueJSON = JSON.stringify(newValue)
-		contentItemMethods.setFieldValue({ value: valueJSON })
-  }
-  
-  // listen for simple-mde resizing events
-  useEffect(() => {
-    const mdeSizeElm = document.querySelector<HTMLElement>(".CodeMirror-sizer")
-    if (!isVisible || !mdeSizeElm) return 
-    const observer = new ResizeObserver((entries) => {
-      const entry = entries[0]
-      if (!entry) return
-      setMarkdownHeight(entry.contentRect.height)
-    })
-    observer.observe(mdeSizeElm)
+	const onChange = (value: string) => {
+		contentItemMethods.setFieldValue({ value })
+	}
 
-    return () => observer.disconnect()
-  }, [isVisible])
+	// listen for simple-mde resizing events
+	useEffect(() => {
+		const mdeSizeElm = document.querySelector<HTMLElement>(".CodeMirror-sizer")
+		if (!isVisible || !mdeSizeElm) return
+		const observer = new ResizeObserver((entries) => {
+			const entry = entries[0]
+			if (!entry) return
+			setMarkdownHeight(entry.contentRect.height)
+		})
+		observer.observe(mdeSizeElm)
 
-  // when the markdown height of the simple-mde changes, then adjust the height of iframe
-  // and offset the height by 200 so the element can scroll all the way back up
-  useEffect(() => {
-    // min height for the editor is 500
-    const height  = markdownHeight < 500 ? 500 : markdownHeight + 200
-    setHeight({ height })
-  }, [ markdownHeight ])
+		return () => observer.disconnect()
+	}, [isVisible])
 
-  return (
-    <div ref={containerRef} className=" bg-white">
-      <SimpleMDE id="simple-mde" value={markdownValues} onChange={onChange} />
-    </div>
-  );
+	// when the markdown height of the simple-mde changes, then adjust the height of iframe
+	// and offset the height by 200 so the element can scroll all the way back up
+	useEffect(() => {
+		// min height for the editor is 500
+		const height = markdownHeight < 500 ? 500 : markdownHeight + 200
+		setHeight({ height })
+	}, [markdownHeight])
 
+	return (
+		<div ref={containerRef} className=" bg-white">
+			<SimpleMDE id="simple-mde" value={markdownValues} onChange={onChange} />
+		</div>
+	)
 }
-
 
 export default MarkdownEditor
